34e066afd6f61e7efb3980909d031b21cf554c08
Jay Hall <jay@alpacalypse.com>
2017-08-31 10:21:16 +1000

    finished app, moving on and forward

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -12,6 +12,10 @@ class MainVC: UIViewController {
 
     @IBOutlet weak var wageTxt: CurrencyTxtField!
     @IBOutlet weak var priceTxt: CurrencyTxtField!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var hoursLbl: UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,10 +29,26 @@ class MainVC: UIViewController {
         wageTxt.inputAccessoryView = calcBtn
         priceTxt.inputAccessoryView = calcBtn
         
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+        
     }
     @objc func calculate() {
-        print("we got here")
-        
+        if let wageTxt = wageTxt.text, let priceTxt = priceTxt.text {
+            if let wage = Double(wageTxt), let price = Double(priceTxt){
+            view.endEditing(true)
+            resultLbl.isHidden = false
+            hoursLbl.isHidden = false
+            resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
+    }
+    
+    @IBAction func clearCalculatorPressed(_ sender: Any) {
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+        wageTxt.text = ""
+        priceTxt.text = ""
     }
 }
 
--- window-shopper/window-shopper/View/CurrencyTxtField.swift
+++ window-shopper/window-shopper/View/CurrencyTxtField.swift
@@ -12,6 +12,19 @@ import UIKit
 
 class CurrencyTxtField: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 0.3079516267)
+        currencyLbl.textAlignment = .center
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -24,6 +37,7 @@ class CurrencyTxtField: UITextField {
         layer.cornerRadius = 5.0
         textAlignment = .center
         
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
